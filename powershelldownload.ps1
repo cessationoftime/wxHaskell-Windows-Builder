307c110a55de8d9590d73f595218dca85d308eb5
cvanvranken <christopher_vanvranken@urmc.rochester.edu>
2014-11-28 10:18:44 -0500

    now skipping download if already downloaded

--- powershelldownload.ps1
+++ powershelldownload.ps1
@@ -1,32 +1,36 @@
-
 $wxWidgetsVersion="3.0.2"
 $wxWidgetsZip="wxWidgets-$wxWidgetsVersion.zip"
 
-#setup parameters for Sourceforge download
-$timestamp=[Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
-$parameterR="http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwxwindows%2Ffiles%2F$wxWidgetsVersion%2F$wxWidgetsZip%2Fdownload"
-$parameters="?r=$parameterR&use_mirror=autoselect&ts=$timestamp"
-$source = "http://downloads.sourceforge.net/project/wxwindows/$wxWidgetsVersion/$wxWidgetsZip$parameters"
-$destination = "$PSScriptRoot/build"
-
-#create the build directory
-New-Item $destination -ItemType directory 
-
-
-
-Function DownloadWX
+Function DownloadWX ($zipfile)
 {
-	#download from sourceforge
-	echo "Downloading from SourceForge: $wxWidgetsZip"
-	Invoke-WebRequest $source -OutFile "$destination\$wxWidgetsZip"
+    if (!(Test-Path $zipfile)){
+	
+		#setup parameters for Sourceforge download
+		$timestamp=[Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
+		$parameterR="http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwxwindows%2Ffiles%2F$wxWidgetsVersion%2F$wxWidgetsZip%2Fdownload"
+		$parameters="?r=$parameterR&use_mirror=autoselect&ts=$timestamp"
+		$source = "http://downloads.sourceforge.net/project/wxwindows/$wxWidgetsVersion/$wxWidgetsZip$parameters"
+	
+	    # if File does not exist
+		# download from sourceforge
+		echo "Downloading from SourceForge: $wxWidgetsZip"
+		Invoke-WebRequest $source -OutFile $zipfile
+	}
+}
+
+$buildDir = "$PSScriptRoot/build"
+
+#If !exists Then create build directory
+if (!(Test-Path $buildDir)){
+	New-Item $buildDir -ItemType directory 
 }
 
 Function UnzipWX
 {
-    unzip "$destination/$wxWidgetsZip" -d "$destination/wxWidgets"
+    unzip "$buildDir/$wxWidgetsZip" -d "$buildDir/wxWidgets"
 }
 
-DownloadWX
+DownloadWX "$buildDir\$wxWidgetsZip"
 UnzipWX
 
 
